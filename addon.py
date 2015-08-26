f5fe4c3eaa7761eea1bf7068d1616b95f1e19960
Stephen Thirlwall <sdt@dr.com>
2015-08-26 22:30:20 +1000

    Try again using exec instead of call

--- addon.py
+++ addon.py
@@ -1,6 +1,6 @@
 # This python script just executes its command-line.
 # Used as a trampoline for kodi to execute an external command.
-import subprocess
+import os
 import sys
 
-subprocess.call(sys.argv[1:])
+os.execv(sys.argv[1], sys.argv[1:])
